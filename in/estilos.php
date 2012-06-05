a32cdae0776eb4b9020f4279c06d22a3a57bbdc2
Raphael Fabeni <rapha.fabeni@gmail.com>
2012-06-05 20:44:10 -0300

    Ajuste CSS

--- in/estilos.php
+++ in/estilos.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/grid-960.css">
+<link rel="stylesheet" href="css/grid-1140.css">
 
 <script src="js/libs/modernizr-2.5.3.min.js"></script>
