b9e5db1e81abb0a8edc6d99878a226ff4db9d8b0
Raphael Fabeni <rapha.fabeni@gmail.com>
2012-11-27 15:53:32 -0200

    Ajustes no CSS IE7 | Favicon e Respond adicionados | Novos metas adicionados

--- in/estilos.php
+++ in/estilos.php
@@ -2,3 +2,4 @@
 <link rel="stylesheet" href="css/grid-960.css">
 
 <script src="js/libs/modernizr-2.5.3.min.js"></script>
+<script src="js/libs/respond.min.js"></script>
--- in/meta.php
+++ in/meta.php
@@ -1,4 +1,6 @@
 <meta name="description" content="">
+<meta name="keywords" content=""> 
+<meta name="author" content="">
 <meta name="viewport" content="width=device-width">
 
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
