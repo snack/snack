6edb2e6f63ac4ff03a65f42ebefe0367dfdf1b6b
Raphael Fabeni <rapha.fabeni@gmail.com>
2012-12-05 16:42:18 -0200

    Ajustes grids 1140 / 960

--- in/estilos.php
+++ in/estilos.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="css/normalize.min.css">
 <link rel="stylesheet" href="css/main.css">
-<link rel="stylesheet" href="css/grid-1140.css">
+<link rel="stylesheet" href="css/grid-960.css">
 
 <!-- modernizr -->
 <script src="js/libs/modernizr-2.6.2.min.js"></script>
