37eac97f8f67054d96254cd30951e52a3e399974
Raphael Fabeni <rapha.fabeni@gmail.com>
2013-02-06 14:47:56 -0200

    Ajustes nos favicons e inserção das chamadas no arquivo 'meta.php'

--- in/meta.php
+++ in/meta.php
@@ -3,4 +3,8 @@
 <meta name="author" content="">
 <meta name="viewport" content="width=device-width">
 
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico">
+<link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" href="apple-touch-icon.png">
