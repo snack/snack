5baadf35e67158143f6e9d9240e42ce81e068594
Raphael Fabeni <rapha.fabeni@gmail.com>
2013-10-15 16:32:03 -0300

    Finaliza ajustes finais da nova versao

--- styleguide/lib/bootstrap.php
+++ styleguide/lib/bootstrap.php
@@ -11,7 +11,7 @@ $twig = new Twig_Environment($loader);
 //Project Configuration
 $container = new A2boilerplate\Container\Pimple();
 $container['project'] = 'Nome do Projeto';
-$container['cms'] = 'WordPress/A2siteBox';
+$container['cms'] = 'A2siteBox/WordPress';
 $container['browsers'] = 'Firefox, Chrome, IE8+';
 $container['styleguide_url'] = '/styleguide';
 $container['meta_description'] = 'Descrição da meta tag description.';
