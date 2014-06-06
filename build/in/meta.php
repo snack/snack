e99d55663b17672786e2a2152d3ef0c08906c97f
raphaelfabeni <rapha.fabeni@gmail.com>
2014-06-06 10:59:40 -0300

    Add default meta tags for Facebook and Twitter cards

--- build/in/meta.php
+++ build/in/meta.php
@@ -7,3 +7,19 @@
 <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
 <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
 <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+<!-- Facebook -->
+<meta property="fb:app_id" content="ID_APP" />
+<meta property="fb:admins" content="100003036613503" />
+<meta property="og:site_name" content="Nome do Projeto"/>
+<meta property="og:title" content="Título do Projeto - Nome do Projeto" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="URL_do_site" />
+<meta property="og:image" content="URL_da_imagem" />
+
+<!-- Twitter -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@perfildotwitter">
+<meta name="twitter:title" content="">
+<meta name="twitter:description" content="">
+<meta name="twitter:image:src" content="URL_da_imagem">
