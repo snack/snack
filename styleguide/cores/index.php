9da703de55b7d011e8fa2697673307e36dacecef
Willian Carminato <williancarminato@gmail.com>
2014-07-25 19:07:18 -0300

    Update modules colors and layouts

--- styleguide/cores/index.php
+++ styleguide/cores/index.php
@@ -2,14 +2,14 @@
 
 require_once __DIR__ . '/../lib/bootstrap.php';
 
-use A2boilerplate\Iterator\BoilerplateDirectoryIterator;
+use Symfony\Component\Yaml\Parser as YamlParser;
 
-//Module Path to Iterate
-$module_path = __DIR__ . '/modules';
-$iterator = new BoilerplateDirectoryIterator($module_path);
+//YAML Parser
+$yaml = new YamlParser();
 
 //Put module name in the container initialized on bootstrap.php
 $container['module'] = "Cores";
 $container['body_class'] = "colors";
+$colors = $yaml->parse(file_get_contents(__DIR__.'/colors.yml'));
 
-echo $twig->render('modules.html.twig', array('iterator' => $iterator, 'container' => $container));
+echo $twig->render('/modules/colors.html.twig', array('colors' => $colors, 'container' => $container));
