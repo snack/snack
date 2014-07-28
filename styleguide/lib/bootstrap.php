2c53f1cf209473f2dd034e998da53ccbd25428a5
Willian Carminato <williancarminato@gmail.com>
2014-07-28 18:52:02 -0300

    automating the creation of sidebar

--- styleguide/lib/bootstrap.php
+++ styleguide/lib/bootstrap.php
@@ -2,17 +2,18 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Symfony\Component\Yaml\Parser as YamlParser;
+
 //Twig Configuration
 $templates_path = __DIR__ . '/layout';
 
 $loader = new Twig_Loader_Filesystem($templates_path);
 $twig = new Twig_Environment($loader);
 
+//YAML Parser
+$yaml = new YamlParser();
+$project = $yaml->parse(file_get_contents(__DIR__.'/../project.yml'));
+
 //Project Configuration
 $container = new A2boilerplate\Container\Pimple();
-$container['project'] = 'Nome do Projeto';
-$container['cms'] = 'A2siteBox/WordPress';
-$container['browsers'] = 'Firefox, Chrome, IE8+';
-$container['styleguide_url'] = '/styleguide';
-$container['meta_description'] = 'Descrição da meta tag description.';
-$container['meta_keywords'] = 'Tags da meta taga keywords.';
+$container['project'] = $project;
