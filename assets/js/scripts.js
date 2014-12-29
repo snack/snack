2469a16cd028ea7fd9e7333de8b3874e5ab22c71
airton <airtonvancin@gmail.com>
2014-12-29 17:09:41 -0200

    Montagem da seção de cases e footer e adicionado meta tags

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,7 @@
-jQuery(document).ready(function($) {
-});
+(function(){
+    $('.social-share a').click(function(e){
+        e.preventDefault();
+        var url = $(this).attr('href');
+        window.open(url,'targetWindow', 'toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=600, height=350');
+    });
+})();
