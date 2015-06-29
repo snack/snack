86b0b04c55691e756bdf735ed618eb985d27c278
Guilherme Lima <guilherme.lima@a2comunicacao.com.br>
2015-06-29 10:46:49 -0300

    Adição seção navegação do styleguide com o modulo de menu em tabs

---
+++ styleguide/navegacao/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../lib/bootstrap.php';
+
+use Symfony\Component\Yaml\Parser as YamlParser;
+
+$loader->addPath(__DIR__.'/modules');
+$loader->addPath(__DIR__.'/template');
+
+//YAML Parser
+$yaml = new YamlParser();
+
+//Put module name in the container initialized on bootstrap.php
+$container['module'] = "Navegação";
+$container['body_class'] = "navegacao";
+$modules_list = $yaml->parse(file_get_contents(__DIR__.'/modules_list_config.yml'));
+
+echo $twig->render('/modulestemplate.html.twig', array('modules_list' => $modules_list, 'container' => $container));
